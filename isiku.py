c13e5e23ca2d95ed0be3d380867df3886ec0c68c
Loginov10 <120027474+Loginov10@users.noreply.github.com>
2023-02-07 13:44:51 +0200

    Update isiku.py

--- isiku.py
+++ isiku.py
@@ -1,4 +1,4 @@
-
+from OmaModul import *
 arvud=[]
 isikukoodid=[]
 while True:
@@ -15,26 +15,11 @@ while True:
            arvud.append(isik)
         else:
             print("Esimene sümbol on õige")
-            i2=isik_list[2]+isik_list[1]        #aasta     #склеиваем значения
-            i3=int(isik_list[3]+isik_list[4])   #kuu
-            i4=int(isik_list[5]+isik_list[6])   #päev
-            if (int(i3)<1 or int(i3)>13) and (int(i4)<1 or int(i4)>31):
-                print("Vale sümbolid")
-                arvud.append(isik)
+            spaev=Sunnipaev(isik_list)
+            if Sunnipaev=="Viga":
+                arvud.apppend(isik)
             else:
-               if i==1 or i==2:
-                    yy="18"
-               elif i==3 or i==4:
-                    yy="19"
-               elif i==5 or i==6:
-                   yy="20"
-               spaev=str(i4)+"."+str(i3)+"."+yy+str(i2)
-               print(f"sünnipäev on",spaev)
-               print(f"Viimane number:{isik_list[-1]}")
-               if i in [2,4,6]:
-                   print("naine")
-               if i in [1,3,5]:
-                   print("mees")
+                #
                a=[1,2,3,4,5,6,7,8,9,1]
                a2=[3,4,5,6,7,8,9,1,2,3]
        
@@ -72,43 +57,18 @@ while True:
                p3=c11-p2
                print(p3)
 
-               hhh=int(isik_list[8]+isik_list[9]+isik_list[10])
-               if 1<=hhh<=10:
-                    h="kuresaare haigla"
-               elif 11<=hhh<=19:
-                    h="Tartu Ülikooli Naistekliinik, Tartumaa, Tartu"
-               elif 21<=hhh<=220:
-                    h=" Ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, "
-               elif 221<=hhh<=270:
-                    h="Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi"
-               elif 271<=hhh<=370:
-                    h=" Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla"
-               elif 371<=hhh<=420:
-                    h="Narva Haigla"
-               elif 421<=hhh<=470:
-                    h="Pärnu Haigla"
-               elif 471<=hhh<=490:
-                    h="Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla"
-               elif 491<=hhh<=520:
-                    h="Järvamaa Haigla (Paide)"
-               elif 521<=hhh<=570:
-                    h="Rakvere, Tapa haigla"
-               elif 571<=hhh<=600:
-                    h="Valga Haigla "
-               elif 601<=hhh<=650:
-                    h="Viljandi Haigla"
-               elif 651<=hhh<=700:
-                    h="Lõuna-Eesti Haigla (Võru), Põlva Haigla "
-               if int(isik_list[0])%2==0:
-                   sugu="naine"
-               else:
-                   sugu="mees"
+               h=Sunnikoht(hhh)
+
+               sugu=Sugu(isik_list)
                print(f"See in {sugu}, sünnipäev {spaev}. ta on sündinud {h}")
                isikukoodid.append(isik)
                break
 print(isikukoodid)
+isikukoodid=naised_mehed(isikukoodid) #sort alguses naised ja pärast mehed
+print(isikukoodid)
 arvud=list(map(int,arvud))  #map - преобразует в новый формат
 arvud.sort()
 print(arvud)
+
 
 
