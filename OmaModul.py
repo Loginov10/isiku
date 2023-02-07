39c0494c6f7388e5426be0b16a58867d0a69c2a9
Loginov10 <120027474+Loginov10@users.noreply.github.com>
2023-02-07 13:43:32 +0200

    Add files via upload

---
+++ OmaModul.py
@@ -0,0 +1,92 @@
+def Sugu(isik_list:list):->str:
+    """Esimise tahe järgi määreme sugu
+    :param list isik_list:Järjend isikukoodi numbridest
+    :rtype: str
+    """
+    if int(isik_list[0])%2==0:
+         sugu="naine"
+    else:
+         sugu="mees"
+    return sugu
+
+def Sunnikoht(a:int)->str:
+    """Esimise tahe järgi määreme Sunnikoht
+    :param list isik_list:
+    :rtype: str
+    """
+    hhh=int(isik_list[8]+isik_list[9]+isik_list[10])
+               if 1<=a<=10:
+                    h="kuresaare haigla"
+               elif 11<=a<=19:
+                    h="Tartu Ülikooli Naistekliinik, Tartumaa, Tartu"
+               elif 21<=a<=220:
+                    h=" Ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, "
+               elif 221<=a<=270:
+                    h="Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi"
+               elif 271<=a<=370:
+                    h=" Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla"
+               elif 371<=a<=420:
+                    h="Narva Haigla"
+               elif 421<=a<=470:
+                    h="Pärnu Haigla"
+               elif 471<=a<=490:
+                    h="Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla"
+               elif 491<=a<=520:
+                    h="Järvamaa Haigla (Paide)"
+               elif 521<=a<=570:
+                    h="Rakvere, Tapa haigla"
+               elif 571<=a<=600:
+                    h="Valga Haigla "
+               elif 601<=a<=650:
+                    h="Viljandi Haigla"
+               elif 651<=a<=700:
+                    h="Lõuna-Eesti Haigla (Võru), Põlva Haigla "
+               else:
+                   h="____"
+               return h
+
+def Sunnipaev(ik_list:list):->str:
+    """
+    :param:
+    :rtype: str
+    """
+            s1=int(ik_list[0])
+            i2=isik_list[2]+isik_list[1]        #aasta     #????????? ????????
+            i3=int(isik_list[3]+isik_list[4])   #kuu
+            i4=int(isik_list[5]+isik_list[6])   #päev
+            if (int(i3)<1 or int(i3)>13) and (int(i4)<1 or int(i4)>31):
+                spaev="Viga"
+                #arvud.append(isik)
+            else:
+               if i==1 or i==2:
+                    yy="18"
+               elif i==3 or i==4:
+                    yy="19"
+               elif i==5 or i==6:
+                   yy="20"
+               spaev=str(i4)+"."+str(i3)+"."+yy+str(i2)
+               print(f"sünnipäev on",spaev)
+               print(f"Viimane number:{isik_list[-1]}")
+               if i in [2,4,6]:
+                   print("naine")
+               if i in [1,3,5]:
+                   print("mees")
+            return spaev
+
+def naised_mehed(isikukoodid:list):
+    """
+    :rtype: list
+    """
+
+    naised=[]
+    mehed=[]
+    for kood in ikoodid:
+        isik_list=list(kood)
+        sugu=Sugu(isik_list)
+        if sugu=="naine"
+            naised.append(kood)
+        else:
+            mehed.append(kood)
+    ikoodid.clear()
+    ikoodid.extend(naised)
+    ikoodid.extend(mehed)
